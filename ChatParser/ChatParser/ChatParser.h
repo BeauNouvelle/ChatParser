7d005229d1e45f756ee3f8b7e0c8f9bc4c8a71ec
Beau Nouvelle <casuist@icloud.com>
2016-01-16 18:54:09 +1100

    Created ChatParser Framework Project

---
+++ ChatParser/ChatParser/ChatParser.h
@@ -0,0 +1,19 @@
+//
+//  ChatParser.h
+//  ChatParser
+//
+//  Created by Beau Young on 16/01/2016.
+//  Copyright © 2016 Beau Nouvelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChatParser.
+FOUNDATION_EXPORT double ChatParserVersionNumber;
+
+//! Project version string for ChatParser.
+FOUNDATION_EXPORT const unsigned char ChatParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatParser/PublicHeader.h>
+
+
